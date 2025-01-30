67bf2167960972eef451c6de2f85d19eb9662ad5
Anis AKNOUCHE <aknouchea@localhost.lip6.fr>
2025-01-30 15:35:40 +0100

    update of entrypoint

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -10,103 +10,104 @@ def load_yaml(file_path):
 
 
 def starts_load_data():
-        load_data_yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/load_data_input_config.yaml"
-        load_data_yaml = load_yaml(load_data_yaml_file_path)
-        load_data_yaml_args = load_data_yaml.get("load_data_args", {})
+        yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/model_configs.yaml"
+        yaml_file = load_yaml(yaml_file_path)
+        yaml_args = yaml_file.get("load_data_args", {})
         
-        load_data_parser = argparse.ArgumentParser("Load Data Parser")
-        load_data_parser.add_argument('--dataset_name', type=str)
-        load_data_parser.add_argument('--object_to_predict', type=str)
-        load_data_parser.add_argument('--random_state_index', type=int)
-        load_data_parser.add_argument('--neg_strategy', type=str, default=load_data_yaml_args.get('neg_strategy'))
-        load_data_parser.add_argument('--generate_syntactic_embeddings', action='store_true', default=False)
-        load_data_parser.add_argument('--generate_semantic_embeddings', action='store_true', default=False)
+        parser = argparse.ArgumentParser("Load Data Parser")
+        parser.add_argument('--dataset_name', type=str)
+        parser.add_argument('--object_to_predict', type=str)
+        parser.add_argument('--random_state_index', type=int)
+        parser.add_argument('--neg_strategy', type=str, default=yaml_args.get('neg_strategy'))
+        parser.add_argument('--generate_syntactic_embeddings', action='store_true', default=False)
+        parser.add_argument('--generate_semantic_embeddings', action='store_true', default=False)
         
-        load_data_args, _ = load_data_parser.parse_known_args()
-        load_data.main(load_data_args)
+        args, _ = parser.parse_known_args()
+        load_data.main(args)
 
 def starts_random_model():
-    random_model_yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/random_model_input_config.yaml"
-    random_model_yaml = load_yaml(random_model_yaml_file_path)
-    random_model_yaml_args = random_model_yaml.get("random_model_args", {})
+    yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/model_configs.yaml"
+    yaml_file = load_yaml(yaml_file_path)
+    yaml_args = yaml_file.get("random_model_args", {})
 
-    random_model_parser = argparse.ArgumentParser("Random Model Parser")
-    random_model_parser.add_argument('--dataset_name', type=str)
-    random_model_parser.add_argument('--object_to_predict', type=str)
-    random_model_parser.add_argument('--random_state_index', type=int)
-    random_model_parser.add_argument('--top_k', type=int, default=random_model_yaml_args.get('top_k'))
+    parser = argparse.ArgumentParser("Random Model Parser")
+    parser.add_argument('--dataset_name', type=str)
+    parser.add_argument('--object_to_predict', type=str)
+    parser.add_argument('--random_state_index', type=int)
+    parser.add_argument('--top_k', type=int, default=yaml_args.get('top_k'))
 
-    random_model_parser_args, _ = random_model_parser.parse_known_args()
-    random_model.main(random_model_parser_args)
+    args, _ = parser.parse_known_args()
+    random_model.main(args)
 
 def starts_syntactic_model():
 
-    syntactic_model_yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/syntactic_model_input_config.yaml"
-    syntactic_model_yaml = load_yaml(syntactic_model_yaml_file_path)
-    syntactic_model_yaml_args = syntactic_model_yaml.get("syntactic_model_args", {})
+    yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/model_configs.yaml"
+    yaml_file = load_yaml(yaml_file_path)
+    yaml_args = yaml_file.get("syntactic_model_args", {})
 
-    syntactic_model_parser = argparse.ArgumentParser("Syntactic Model Parser")
-    syntactic_model_parser.add_argument('--dataset_name', type=str)
-    syntactic_model_parser.add_argument('--object_to_predict', type=str)
-    syntactic_model_parser.add_argument('--random_state_index', type=int)
-    syntactic_model_parser.add_argument('--top_k', type=int, default=syntactic_model_yaml_args.get('top_k'))
-    syntactic_model_parser.add_argument('--nb_epochs', type=int, default=syntactic_model_yaml_args.get('nb_epochs'))
+    parser = argparse.ArgumentParser("Syntactic Model Parser")
+    parser.add_argument('--dataset_name', type=str)
+    parser.add_argument('--object_to_predict', type=str)
+    parser.add_argument('--random_state_index', type=int)
+    parser.add_argument('--top_k', type=int, default=yaml_args.get('top_k'))
+    parser.add_argument('--nb_epochs', type=int, default=yaml_args.get('nb_epochs'))
     
-    syntactic_model_args, _ = syntactic_model_parser.parse_known_args()
-    syntactic_model.main(syntactic_model_args)
+    args, _ = parser.parse_known_args()
+    syntactic_model.main(args)
+
 
 def starts_semantic_model():
-        semantic_model_yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/semantic_model_input_config.yaml"
-        semantic_model_yaml = load_yaml(semantic_model_yaml_file_path)
-        semantic_model_yaml_args = semantic_model_yaml.get("semantic_model_args", {})
+    yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/model_configs.yaml"
+    yaml_file = load_yaml(yaml_file_path)
+    yaml_args = yaml_file.get("semantic_model_args", {})
+
+    parser = argparse.ArgumentParser("Semantic Model Parser")
+    parser.add_argument('--dataset_name', type=str)
+    parser.add_argument('--object_to_predict', type=str)
+    parser.add_argument('--random_state_index', type=int)
+    parser.add_argument('--top_k', type=int, default=yaml_args.get('top_k'))
+    parser.add_argument('--nb_epochs', type=int, default=yaml_args.get('nb_epochs'))
     
-        sem_model_parser = argparse.ArgumentParser("Semantic Model Parser")
-        sem_model_parser.add_argument('--dataset_name', type=str)
-        sem_model_parser.add_argument('--object_to_predict', type=str)
-        sem_model_parser.add_argument('--random_state_index', type=int)
-        sem_model_parser.add_argument('--top_k', type=int, default=semantic_model_yaml_args.get('top_k'))
-        sem_model_parser.add_argument('--nb_epochs', type=int, default=semantic_model_yaml_args.get('nb_epochs'))
-        
-        sem_model_args, _ = sem_model_parser.parse_known_args()
-        semantic_model.main(sem_model_args)
+    args, _ = parser.parse_known_args()
+    semantic_model.main(args)
 
 
 def starts_graph_model():
-        graph_model_yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/graph_model_input_config.yaml"
-        graph_model_yaml = load_yaml(graph_model_yaml_file_path)
-        graph_model_yaml_args = graph_model_yaml.get("graph_model_args", {})
-    
-        graph_model_parser = argparse.ArgumentParser("Graph Model Parser")
-        graph_model_parser.add_argument('--dataset_name', type=str)
-        graph_model_parser.add_argument('--object_to_predict', type=str)
-        graph_model_parser.add_argument('--random_state_index', type=int)
-        graph_model_parser.add_argument('--max_epochs', type=int, default=graph_model_yaml_args.get('max_epochs'))
-        graph_model_parser.add_argument('--graph_dim_embeddings', type=int, default=graph_model_yaml_args.get('graph_dim_embeddings'))
-        graph_model_parser.add_argument('--learning_rate', type=float, default=graph_model_yaml_args.get('learning_rate'))
-    
-        graph_model_args, _ = graph_model_parser.parse_known_args()
-        graph_model.main(graph_model_args)
+    yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/model_configs.yaml"
+    yaml_file = load_yaml(yaml_file_path)
+    yaml_args = yaml_file.get("graph_model_args", {})
+
+    parser = argparse.ArgumentParser("Graph Model Parser")
+    parser.add_argument('--dataset_name', type=str)
+    parser.add_argument('--object_to_predict', type=str)
+    parser.add_argument('--random_state_index', type=int)
+    parser.add_argument('--max_epochs', type=int, default=yaml_args.get('max_epochs'))
+    parser.add_argument('--graph_dim_embeddings', type=int, default=yaml_args.get('graph_dim_embeddings'))
+    parser.add_argument('--learning_rate', type=float, default=yaml_args.get('learning_rate'))
+
+    args, _ = arser.parse_known_args()
+    graph_model.main(args)
 
 
 def starts_hybrid_model():
-    hybrid_model_yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/hybrid_model_input_config.yaml"
-    hybrid_model_yaml = load_yaml(hybrid_model_yaml_file_path)
-    hybrid_model_yaml_args = hybrid_model_yaml.get("hybrid_model_args", {})
+    yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/model_configs.yaml"
+    yaml_file = load_yaml(yaml_file_path)
+    yaml_args = yaml_file.get("hybrid_model_args", {})
 
-    hybrid_model_parser = argparse.ArgumentParser("Hybrid Model Parser")
-    hybrid_model_parser.add_argument('--dataset_name', type=str)
-    hybrid_model_parser.add_argument('--object_to_predict', type=str)
-    hybrid_model_parser.add_argument('--random_state_index', type=int)
-    hybrid_model_parser.add_argument('--batch_size', type=int, default=hybrid_model_yaml_args.get('batch_size'))
-    hybrid_model_parser.add_argument('--num_workers', type=int, default=hybrid_model_yaml_args.get('num_workers'))
-    hybrid_model_parser.add_argument('--nb_epochs', type=int, default=hybrid_model_yaml_args.get('nb_epochs'))
-    hybrid_model_parser.add_argument('--num_classes', type=int, default=hybrid_model_yaml_args.get('num_classes'))
-    hybrid_model_parser.add_argument('--learning_rate', type=float, default=hybrid_model_yaml_args.get('learning_rate'))
-    hybrid_model_parser.add_argument('--hidden_layer_dim', type=int, default=hybrid_model_yaml_args.get('hidden_layer_dim'))
-    hybrid_model_parser.add_argument('--top_k', type=int, default=hybrid_model_yaml_args.get('top_k'))
+    parser = argparse.ArgumentParser("Hybrid Model Parser")
+    parser.add_argument('--dataset_name', type=str)
+    parser.add_argument('--object_to_predict', type=str)
+    parser.add_argument('--random_state_index', type=int)
+    parser.add_argument('--batch_size', type=int, default=yaml_args.get('batch_size'))
+    parser.add_argument('--num_workers', type=int, default=yaml_args.get('num_workers'))
+    parser.add_argument('--nb_epochs', type=int, default=yaml_args.get('nb_epochs'))
+    parser.add_argument('--num_classes', type=int, default=yaml_args.get('num_classes'))
+    parser.add_argument('--learning_rate', type=float, default=yaml_args.get('learning_rate'))
+    parser.add_argument('--hidden_layer_dim', type=int, default=yaml_args.get('hidden_layer_dim'))
+    parser.add_argument('--top_k', type=int, default=yaml_args.get('top_k'))
 
-    hybrid_model_args, _ = hybrid_model_parser.parse_known_args()
-    hybrid_model.main(hybrid_model_args)
+    args, _ = parser.parse_known_args()
+    hybrid_model.main(args)
 
 def starts_hybrid_model_syn_sem_embed_gen():
     yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/model_configs.yaml"
@@ -130,8 +131,8 @@ def starts_hybrid_model_syn_sem_embed_gen():
     
 def starts_hybrid_model_syn_sem_graph_embed_gen():
     yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/model_configs.yaml"
-    yaml = load_yaml(yaml_file_path)
-    yaml_args = yaml.get("hybrid_model_syn_sem_graph_embed_gen_args", {})
+    yaml_file = load_yaml(yaml_file_path)
+    yaml_args = yaml_file.get("hybrid_model_syn_sem_graph_embed_gen_args", {})
 
     parser = argparse.ArgumentParser("Hybrid Model Parser")
     parser.add_argument('--dataset_name', type=str)
@@ -149,25 +150,25 @@ def starts_hybrid_model_syn_sem_graph_embed_gen():
     hybrid_model_syn_sem_graph_embed_gen.main(args)
 
 def starts_hybrid_sim_model():
-    hybrid_sim_model_yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/hybrid_similarity_model_input_config.yaml"
-    hybrid_sim_model_yaml = load_yaml(hybrid_sim_model_yaml_file_path)
-    hybrid_sim_model_yaml_args = hybrid_sim_model_yaml.get("hybrid_similarity_model_args", {})
+    yaml_file_path = "/home/aknouchea/link-prediction-experiments/hybrid-link-prediction/src/input_yaml_config/model_configs.yaml"
+    yaml_file = load_yaml(yaml_file_path)
+    yaml_args = yaml_file.get("hybrid_similarity_model_args", {})
 
-    hybrid_sim_model_parser = argparse.ArgumentParser("Hybrid Similarity Model Parser")
-    hybrid_sim_model_parser.add_argument('--dataset_name', type=str)
-    hybrid_sim_model_parser.add_argument('--object_to_predict', type=str)
-    hybrid_sim_model_parser.add_argument('--random_state_index', type=int)
-    hybrid_sim_model_parser.add_argument('--batch_size', type=int, default=hybrid_sim_model_yaml_args.get('batch_size'))
-    hybrid_sim_model_parser.add_argument('--num_workers', type=int, default=hybrid_sim_model_yaml_args.get('num_workers'))
-    hybrid_sim_model_parser.add_argument('--nb_epochs', type=int, default=hybrid_sim_model_yaml_args.get('nb_epochs'))
-    hybrid_sim_model_parser.add_argument('--num_classes', type=int, default=hybrid_sim_model_yaml_args.get('num_classes'))
-    hybrid_sim_model_parser.add_argument('--learning_rate', type=float, default=hybrid_sim_model_yaml_args.get('learning_rate'))
-    hybrid_sim_model_parser.add_argument('--hidden_layer_dim', type=int, default=hybrid_sim_model_yaml_args.get('hidden_layer_dim'))
-    hybrid_sim_model_parser.add_argument('--top_k', type=int, default=hybrid_sim_model_yaml_args.get('top_k'))
+    parser = argparse.ArgumentParser("Hybrid Similarity Model Parser")
+    parser.add_argument('--dataset_name', type=str)
+    parser.add_argument('--object_to_predict', type=str)
+    parser.add_argument('--random_state_index', type=int)
+    parser.add_argument('--batch_size', type=int, default=yaml_args.get('batch_size'))
+    parser.add_argument('--num_workers', type=int, default=yaml_args.get('num_workers'))
+    parser.add_argument('--nb_epochs', type=int, default=yaml_args.get('nb_epochs'))
+    parser.add_argument('--num_classes', type=int, default=yaml_args.get('num_classes'))
+    parser.add_argument('--learning_rate', type=float, default=yaml_args.get('learning_rate'))
+    parser.add_argument('--hidden_layer_dim', type=int, default=yaml_args.get('hidden_layer_dim'))
+    parser.add_argument('--top_k', type=int, default=yaml_args.get('top_k'))
 
 
-    hybrid_sim_model_parser_args, _ = hybrid_sim_model_parser.parse_known_args()
-    hybrid_similarity_model.main(hybrid_sim_model_parser_args)
+    parser_args, _ = parser.parse_known_args()
+    hybrid_similarity_model.main(parser_args)
 
 
 def are_embeddings_generated(dataset_name, model_name, random_state_index):
