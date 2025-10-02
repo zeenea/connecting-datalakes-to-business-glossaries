9efcf4141c52aa1246f03825dcf620d8627f13b2
AKNOUCHE Anis <ga_aknouche@esi.dz>
2025-10-02 18:48:18 +0200

    debbug

--- src/models/reciprocal_rank_fusion_model.py
+++ src/models/reciprocal_rank_fusion_model.py
@@ -680,10 +680,10 @@ def main(args):
                 device=None
             )
 
-        print(semantic_top_k_suggestions)
-        print(graph_top_k_suggestions)
-        print(hybrid_sem_graph_top_k_suggestions)
-        print(cross_sem_graph_top_k_suggestions)
+        print(semantic_top_k_suggestions[0])
+        print(graph_top_k_suggestions[0])
+        print(hybrid_sem_graph_top_k_suggestions[0])
+        print(cross_sem_graph_top_k_suggestions[0])
 
         logger.info("Compute final ranking with RRF")
 
@@ -695,7 +695,7 @@ def main(args):
             top_k=parameters['top_k']
         )
         top_k_combined_suggestions = top_k_combined_suggestions.type(torch.LongTensor)
-        print(top_k_combined_suggestions)
+        print(top_k_combined_suggestions[0])
 
         logger.info("Compute MRR and Hit@K")
 
