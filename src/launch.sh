c5f1a722a2453d446eb48e1ca34cb5e32e7f205e
AKNOUCHE Anis <ga_aknouche@esi.dz>
2026-04-22 20:03:47 +0200

    update launch file

--- src/launch.sh
+++ src/launch.sh
@@ -4,9 +4,10 @@ random_state_indexes=(42 48 13 31 88 199 98 3 76 99)         # list of random st
 
 
 for random_state_index in "${random_state_indexes[@]}"; do
+    python entrypoint.py --dataset_name=$dataset_name --object_to_annotate=$object_to_annotate --random_state_index=$random_state_index --generate_syntactic_embeddings --enable_syntactic_model
     python entrypoint.py --dataset_name=$dataset_name --object_to_annotate=$object_to_annotate --random_state_index=$random_state_index --generate_semantic_embeddings --enable_semantic_model
+    python entrypoint.py --dataset_name=$dataset_name --object_to_annotate=$object_to_annotate --random_state_index=$random_state_index --generate_semantic_textual_links --enable_binary_classifier_model
     python entrypoint.py --dataset_name=$dataset_name --object_to_annotate=$object_to_annotate --random_state_index=$random_state_index --enable_graph_model
-    python entrypoint.py --dataset_name=$dataset_name --object_to_annotate=$object_to_annotate --random_state_index=$random_state_index --enable_binary_classifier_model
 
     python entrypoint.py --dataset_name=$dataset_name --object_to_annotate=$object_to_annotate --random_state_index=$random_state_index --enable_hybrid_model_sem_graph_embedding_learning
     python entrypoint.py --dataset_name=$dataset_name --object_to_annotate=$object_to_annotate --random_state_index=$random_state_index --enable_hybrid_model_syn_graph_similarity_learning
